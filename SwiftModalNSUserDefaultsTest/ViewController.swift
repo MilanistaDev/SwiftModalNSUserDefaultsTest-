7e010854958fd291b42b353913e653de3bc7471b
MilanistaDev <milanista224@gmail.com>
2014-12-22 12:26:49 +0900

    Initial Commit

---
+++ SwiftModalNSUserDefaultsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftModalNSUserDefaultsTest
+//
+//  Created by 麻生 拓弥 on 2014/12/22.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
